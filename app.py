946755147e1d2ae09083ecd4e6ce96dc6e20539b
Hitanshu Satpathy <120390701+geekofycoder@users.noreply.github.com>
2023-11-19 16:55:03 +0000

    updation

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 #Writes all flask related codes in here
-from flask import Flask,render_template
-app=Flask(__name__)
-@app.route('/')#home route
+from flask import Flask, render_template, request
+import pickle
+
+app = Flask(__name__)
+cv=pickle.load("models/cv.pkl")
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template("index.html")
-if __name__=='__main__':
-    app.run(host='0.0.0.0',debug=True)#block to ensure the server only runs if the script is executed directly #debug makes the upadations 
+    text = ""
+    if request.method == 'POST':
+        text = request.form.get('email-content')
+    return render_template('index.html', text=text)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)#block to ensure the server only runs if the script is executed directly #debug makes the upadations 
